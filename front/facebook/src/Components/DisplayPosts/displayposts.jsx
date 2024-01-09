786caf463241aaadad509621012ab4eca34f1ba0
unknown <matanhaimov2@gmail.com>
2024-01-09 07:57:19 +0200

    friends & online friends are done, waiting for feed ...

---
+++ front/facebook/src/Components/DisplayPosts/displayposts.jsx
@@ -0,0 +1,78 @@
+import React, { useState, useEffect } from 'react';
+
+
+// SubComponents
+import Post from './subComponent/post';
+
+//CSS
+import './displayposts.css';
+
+// Services
+import { getProfilePost } from '../../Services/profileService';
+import { getAuthenticatedUser } from '../../Services/authService';
+
+
+function DisplayPosts({ profileEmail }) {
+
+    // States
+    const [profilePosts, setProfilePosts] = useState(null); // 
+
+
+    useEffect(() => {
+        
+        const getPostToFacebook = async () => {
+            let data;
+         
+            if(profileEmail) {
+                data = {
+                    "Email" : profileEmail
+                }
+            }
+            else {
+                data = {
+                    "Email" : getAuthenticatedUser()
+                }
+            }
+            
+            if(getAuthenticatedUser()) {
+
+
+                const response = await getProfilePost(data)
+                if(response && response.res===true) { // If the response is true, enter                    
+                    setProfilePosts(response.data)
+
+                }
+                else {
+                    setProfilePosts([])
+                }
+
+            }
+
+        }
+
+        getPostToFacebook();
+    }, [])
+
+
+    return (
+        <div className='displayposts-wrapper-wrapper'>
+            {profilePosts && profilePosts.map((post, i) => (
+                <Post index={i} post={post} />                    
+            ))}      
+
+            {profilePosts && profilePosts.length===0 && (
+                <div className='displaypost-noposts'>
+                    No Posts To Display
+                </div>
+            )}       
+
+            {!profilePosts && (
+                <div>
+                    Loading
+                </div>
+            )}         
+        </div>
+    );
+}
+
+export default DisplayPosts;
